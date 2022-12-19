96a9a8ad535a09d6952c2c50b0d686ba2a1ff64a
nikhilpaspuleti20 <116423567+nikhilpaspuleti20@users.noreply.github.com>
2022-12-19 20:42:04 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,12 @@
+"""
+DATE:12TH NOV 2022
+DATE:12TH DEC 2022
+DAY: MONDAY
+TOPIC: NUMBER SYSTEM
+AUTHOR: nikhil
+"""
+f=1_2.3
+print(f,type(f)) #12.3 float
+# k=1_.3 #SyntaxError:invalid decimal literal 
+# n=1._3 #SyntaxError: invalid decimal literal
+# n=1._3 #SyntaxError: invalid decimal literal
